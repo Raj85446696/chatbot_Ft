2aab9626628d579c789dbac31e72ebecfa33aada
RITU RAJ KUMAR <146192378+Raj85446696@users.noreply.github.com>
2026-04-18 13:16:37 +0530

    Update BASE_API_URL to use HTTPS

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
 
-  BASE_API_URL: 'http://98.130.58.81:8000',
+  BASE_API_URL: 'https://98.130.58.81:8000',
 
   ENDPOINTS: {
     ADMIN_BASE: '/api/admin',
